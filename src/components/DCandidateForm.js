b55f7b955f187894b39fb2e6f9c3c56d9f062cd9
TecnoNX <40416331+TecnoNX@users.noreply.github.com>
2022-01-20 11:30:22 -0400

    app with react

---
+++ src/components/DCandidateForm.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from "react";
+import { Grid, TextField, withStyles, FormControl, InputLabel, Select, MenuItem, Button, FormHelperText } from "@material-ui/core";
+import useForm from "./useForm";
+import { connect } from "react-redux";
+import * as actions from "../actions/dCandidate";
+import { useToasts } from "react-toast-notifications";
+
+const styles = theme => ({
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            minWidth: 230,
+        }
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 230,
+    },
+    smMargin: {
+        margin: theme.spacing(1)
+    }
+})
+
+const initialFieldValues = {
+    title: '',
+    author: '',
+    description: ''
+}
+
+const DCandidateForm = ({ classes, ...props }) => {
+
+    //toast msg.
+    const { addToast } = useToasts()
+
+    //validate()
+    //validate({fullName:'jenny'})
+    const validate = (fieldValues = values) => {
+        let temp = { ...errors }
+        if ('title' in fieldValues)
+            temp.title = fieldValues.title ? "" : "This field is required."
+        if ('author' in fieldValues)
+            temp.author = fieldValues.author ? "" : "This field is required."
+       
+        if ('description' in fieldValues)
+            temp.description = (fieldValues.description) ? "" : "description is not valid."
+        setErrors({
+            ...temp
+        })
+
+        if (fieldValues == values)
+            return Object.values(temp).every(x => x == "")
+    }
+
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        resetForm
+    } = useForm(initialFieldValues, validate, props.setCurrentId)
+
+    //material-ui select
+    const inputLabel = React.useRef(null);
+    const [labelWidth, setLabelWidth] = React.useState(0);
+    React.useEffect(() => {
+        
+    }, []);
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (validate()) {
+            const onSuccess = () => {
+                resetForm()
+                addToast("Submitted successfully", { appearance: 'success' })
+            }
+            if (props.currentId == 0)
+                props.createDCandidate(values, onSuccess)
+            else
+                props.updateDCandidate(props.currentId, values, onSuccess)
+        }
+    }
+
+    useEffect(() => {
+        if (props.currentId != 0) {
+            setValues({
+                ...props.dCandidateList.find(x => x.id == props.currentId)
+            })
+            setErrors({})
+        }
+    }, [props.currentId])
+
+    return (
+        <form autoComplete="off" noValidate className={classes.root} onSubmit={handleSubmit}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <TextField
+                        name="title"
+                        variant="outlined"
+                        label="Titulo"
+                        value={values.title}
+                        onChange={handleInputChange}
+                        {...(errors.title && { error: true, helperText: errors.title })}
+                    />
+                    <TextField
+                        name="description"
+                        variant="outlined"
+                        label="Description"
+                        value={values.description}
+                        onChange={handleInputChange}
+                        {...(errors.description && { error: true, helperText: errors.description })}
+                    />
+                    
+                </Grid>
+                <Grid item xs={6}>
+
+                    <TextField
+                        name="author"
+                        variant="outlined"
+                        label="Autor"
+                        value={values.author}
+                        onChange={handleInputChange}
+                        {...(errors.author && { error: true, helperText: errors.author })}
+                    />
+                   {/* <TextField
+                        name="age"
+                        variant="outlined"
+                        label="Age"
+                        value={values.age}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        name="address"
+                        variant="outlined"
+                        label="Address"
+                        value={values.address}
+                        onChange={handleInputChange}
+                   />*/}
+                    <div>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                            className={classes.smMargin}
+                        >
+                            Submit
+                        </Button>
+                        <Button
+                            variant="contained"
+                            className={classes.smMargin}
+                            onClick={resetForm}
+                        >
+                            Reset
+                        </Button>
+                    </div>
+                </Grid>
+            </Grid>
+        </form>
+    );
+}
+
+
+const mapStateToProps = state => ({
+    dCandidateList: state.dCandidate.list
+})
+
+const mapActionToProps = {
+    createDCandidate: actions.create,
+    updateDCandidate: actions.update
+}
+
+export default connect(mapStateToProps, mapActionToProps)(withStyles(styles)(DCandidateForm));
